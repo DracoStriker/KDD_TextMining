bd63d74e418176b8122e1c2249168409ffacec72
Miguel Nóbrega <lnobrega@ua.pt>
2017-11-23 23:56:07 +0000

    Pre-Processing function +  DocumentTermMatrix
    Added pre-processing function
    Stem is not being used for now
    DocumentTermMatrix tested

--- TextMining/TextMining.R
+++ TextMining/TextMining.R
@@ -1,8 +1,35 @@
 library(tm)
 library(Rstem)
+library(SnowballC)
 
 #setwd('GitHub/KDD_TextMining/TextMining')
+setwd("C:/Users/Miguel Nóbrega/ProjetosGit/KDD_TextMining/TextMining")
 
+#Create the corpus
+mistery.train <- Corpus(DirSource("mistery"), readerControl = list(reader=readPlain, language="en"))
+romance.train <- Corpus(DirSource("romance"), readerControl = list(reader=readPlain, language="en"))
+
+#lenght of folders
+length(mistery.train) #2985
+length(romance.train) #3884
+
+#pre-processing 
+mistery.train.p <- preprocess.simple(mistery.train)
+romance.train.p <- preprocess.simple(romance.train)
+
+#o stemming está a remover palavras que podem ser úteis.. por exemplo dectetive estava a ser substituido por detect..
+
+#creating document-term matrix
+DocumentTermMatrix(mistery.train.p)
+
+#palavras entre 4 e 15 letras, 3 occorencias globais e 2 ocorrencias locais
+DocumentTermMatrix (mistery.train.p, control = list(WordLengths = c(4,15), bounds = list(global=c(3,Inf), local=c(2,Inf))))
+
+dtm <- DocumentTermMatrix(mistery.train.p, control = list (bounds = list(global = c(2,Inf))))
+dim(dtm)
+colnames (dtm) [1:20]
+
+#-------
 txt <- readLines('text/romance/tt0034583_0.txt')
 
 # split text by spaces
@@ -28,3 +55,21 @@ words <- words[words != ""]
 
 # aggregate words
 words <- table(words)
+
+##-----FUNCTIONS--------
+preprocess.simple <- function(d){
+  d<-tm_map (d, content_transformer(tolower))            #all lower case
+  d<-tm_map (d, makewhitespace, "/")
+  d<-tm_map (d, makewhitespace, "@")
+  d<-tm_map (d, makewhitespace, "\\|")
+  d<-tm_map (d, removeWords, stopwords (kind= "en"))     #remove stop words (174)
+  d<-tm_map (d, removePunctuation, preserve_intra_word_dases = TRUE )  #removeponctuation
+  d<-tm_map (d, removeNumbers)    #remove numbers
+  #d<-tm_map (d, stemDocument)     #word stemmer
+  d<-tm_map (d, stripWhitespace)  #remove whitespaces
+}
+
+#function to substitute symbols by whitespaces 
+makewhitespace <- content_transformer(function (d, pattern) 
+    gsub(pattern, " ", d))
+

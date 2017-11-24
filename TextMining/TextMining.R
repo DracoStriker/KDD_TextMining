37ed7109d10c256bc5866617da8b6923574afb7a
Simão Reis <dracostriker@hotmail.com>
2017-11-24 17:36:20 +0000

    Class attribute
    Added class attribute

--- TextMining/TextMining.R
+++ TextMining/TextMining.R
@@ -1,75 +1,84 @@
 library(tm)
-library(Rstem)
 library(SnowballC)
+library(FSelector)
 
 #setwd('GitHub/KDD_TextMining/TextMining')
-setwd("C:/Users/Miguel Nóbrega/ProjetosGit/KDD_TextMining/TextMining")
+#setwd("C:/Users/Miguel N?brega/ProjetosGit/KDD_TextMining/TextMining")
 
-#Create the corpus
+# Substitute symbols by whitespaces 
+makewhitespace <- content_transformer(function (d, pattern) gsub(pattern, " ", d))
+
+# Text cleaning
+preprocess.simple <- function(d) {
+  
+  # words to lower case
+  d <- tm_map (d, content_transformer(tolower))
+  
+  # clean symbols
+  d <- tm_map (d, makewhitespace, "/")
+  d <- tm_map (d, makewhitespace, "@")
+  d <- tm_map (d, makewhitespace, "\\|")
+  
+  # remove stop words (174)
+  d <- tm_map (d, removeWords, stopwords (kind= "en"))
+  
+  # remove punctuation
+  d <- tm_map (d, removePunctuation, preserve_intra_word_dases = TRUE)
+  
+  # remove digits
+  d <- tm_map (d, removeNumbers)
+  
+  # word stemming
+  #d <- tm_map (d, stemDocument)
+  
+  # remove white spaces
+  d <- tm_map (d, stripWhitespace)
+  
+  d
+}
+
+# Create the corpus
 mistery.train <- Corpus(DirSource("mistery"), readerControl = list(reader=readPlain, language="en"))
 romance.train <- Corpus(DirSource("romance"), readerControl = list(reader=readPlain, language="en"))
 
-#lenght of folders
+# Print lenght of folders
 length(mistery.train) #2985
 length(romance.train) #3884
 
-#pre-processing 
-mistery.train.p <- preprocess.simple(mistery.train)
-romance.train.p <- preprocess.simple(romance.train)
+# Pre-processing 
+mistery.train <- preprocess.simple(mistery.train)
+romance.train <- preprocess.simple(romance.train)
 
-#o stemming está a remover palavras que podem ser úteis.. por exemplo dectetive estava a ser substituido por detect..
+# Concat corpus
+overall.train <- Corpus(VectorSource(c(content(mistery.train), content(romance.train))))
 
-#creating document-term matrix
-DocumentTermMatrix(mistery.train.p)
+# Generate document term matrix
+# Words between 4 and 15 letters
+# 3 global occurences
+# 2 local ocurrences
+# TfIdf weight
+dtm <- DocumentTermMatrix(overall.train, control = list(WordLengths = c(4, 15), bounds = list(global=c(3, Inf), local=c(2, Inf)), weighting = weightTfIdf))
 
-#palavras entre 4 e 15 letras, 3 occorencias globais e 2 ocorrencias locais
-DocumentTermMatrix (mistery.train.p, control = list(WordLengths = c(4,15), bounds = list(global=c(3,Inf), local=c(2,Inf))))
+# remove sparse terms (95% sparcity)
+dtm <- removeSparseTerms(dtm, 0.95)
 
-dtm <- DocumentTermMatrix(mistery.train.p, control = list (bounds = list(global = c(2,Inf))))
-dim(dtm)
-colnames (dtm) [1:20]
+# MAtrix statistics
+#dim(dtm)
+#colnames (dtm) [1:20]
 
-#-------
-txt <- readLines('text/romance/tt0034583_0.txt')
+# Document term matrix to dataframe
+train.d <- as.data.frame(as.matrix(dtm))
 
-# split text by spaces
-words <- strsplit(txt, ' ')
+# Generate lexicon
+#lexicon <- names(train.d)
 
-# words to lower case
-words <- sapply(words, tolower)
+# Class vector
+c.vector <- c(c(rep(0, length(mistery.train)), c(rep(1, length(romance.train)))))
 
-# remove stop words
-words <- setdiff(words, stopwords(kind = 'en'))
+# Bind class feature
+train.d <- cbind(train.d, class=c.vector)
 
-# remove punctuation
-words <- gsub('[[:punct:]]+', '', words)
-
-# remove digits
-words <- gsub('[[:digit:]]+', '', words)
-
-# word steming
-words <- wordStem(words, language='english')
-
-# remove empty words
-words <- words[words != ""]
-
-# aggregate words
-words <- table(words)
-
-##-----FUNCTIONS--------
-preprocess.simple <- function(d){
-  d<-tm_map (d, content_transformer(tolower))            #all lower case
-  d<-tm_map (d, makewhitespace, "/")
-  d<-tm_map (d, makewhitespace, "@")
-  d<-tm_map (d, makewhitespace, "\\|")
-  d<-tm_map (d, removeWords, stopwords (kind= "en"))     #remove stop words (174)
-  d<-tm_map (d, removePunctuation, preserve_intra_word_dases = TRUE )  #removeponctuation
-  d<-tm_map (d, removeNumbers)    #remove numbers
-  #d<-tm_map (d, stemDocument)     #word stemmer
-  d<-tm_map (d, stripWhitespace)  #remove whitespaces
-}
-
-#function to substitute symbols by whitespaces 
-makewhitespace <- content_transformer(function (d, pattern) 
-    gsub(pattern, " ", d))
-
+# Information gain
+#info.terms <- information.gain(class ~., train.d)
+#which(info.terms$attr_importance > info.min)
+#rownames(info.terms)

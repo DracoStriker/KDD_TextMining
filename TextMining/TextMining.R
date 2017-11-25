00555ff12431a412bc89b97d6dd68d448d4b92ae
Miguel Nóbrega <lnobrega@ua.pt>
2017-11-25 00:14:07 +0000

    Test set + Dtree
    Data splitted into train and test set before the preprocessing (70%/30%)
    Needed to change the method of stopword. "en" was not working. I changed
    to "english"
    Dtree implemented + confusion matrix
    Big number of FN and FP.

--- TextMining/TextMining.R
+++ TextMining/TextMining.R
@@ -1,6 +1,8 @@
 library(tm)
 library(SnowballC)
 library(FSelector)
+library(quanteda)
+library(caret)
 
 #setwd('GitHub/KDD_TextMining/TextMining')
 #setwd("C:/Users/Miguel N?brega/ProjetosGit/KDD_TextMining/TextMining")
@@ -20,7 +22,7 @@ preprocess.simple <- function(d) {
   d <- tm_map (d, makewhitespace, "\\|")
   
   # remove stop words (174)
-  d <- tm_map (d, removeWords, stopwords (kind= "en"))
+  d <- tm_map (d, removeWords, stopwords("english"))
   
   # remove punctuation
   d <- tm_map (d, removePunctuation, preserve_intra_word_dases = TRUE)
@@ -38,18 +40,32 @@ preprocess.simple <- function(d) {
 }
 
 # Create the corpus
-mistery.train <- Corpus(DirSource("mistery"), readerControl = list(reader=readPlain, language="en"))
-romance.train <- Corpus(DirSource("romance"), readerControl = list(reader=readPlain, language="en"))
+mistery <- Corpus(DirSource("mistery"), readerControl = list(reader=readPlain, language="en"))
+romance <- Corpus(DirSource("romance"), readerControl = list(reader=readPlain, language="en"))
 
-# Print lenght of folders
-length(mistery.train) #2985
-length(romance.train) #3884
+#create Train and Test set for group1
+mistery.ri <- sort(sample(length(mistery), length(mistery)*.7))
+mistery.train<-mistery[c(mistery.ri)]
+mistery.test<-mistery[c(-mistery.ri)]
+
+
+#create Train and Test set for group1
+romance.ri <- sort(sample(length(romance), length(romance)*.7))
+romance.train<-romance[c(romance.ri)]
+romance.test<-romance[c(-romance.ri)] 
+
+# Print lenght of datasets
+length(mistery.train) #2089
+length(mistery.test) #896
+length(romance.train) #2718
+length(romance.test) #1166
+
 
 # Pre-processing 
 mistery.train <- preprocess.simple(mistery.train)
 romance.train <- preprocess.simple(romance.train)
 
-# Concat corpus
+# Concat corpus train
 overall.train <- Corpus(VectorSource(c(content(mistery.train), content(romance.train))))
 
 # Generate document term matrix
@@ -69,23 +85,45 @@ dtm <- removeSparseTerms(dtm, 0.95)
 # Document term matrix to dataframe
 train.d <- as.data.frame(as.matrix(dtm))
                          
-# Shuffle data
-train.d <- train.d[sample(nrow(train.d)),]
 
 # Generate lexicon
 lexicon <- names(train.d)
 
 # Class vector
-c.vector <- c(c(rep(0, length(mistery.train)), c(rep(1, length(romance.train)))))
+train.c <- c(c(rep("Mistery", length(mistery.train)), c(rep("Romance", length(romance.train)))))
 
 # Bind class feature
-train.d <- cbind(train.d, class=c.vector)
+train.dc <- cbind(train.d, class=train.c)
+
+# Shuffle data
+train.dc <- train.dc[sample(nrow(train.dc)),]
 
 # Information gain
 #info.terms <- information.gain(class ~., train.d)
 #which(info.terms$attr_importance > info.min)
 #rownames(info.terms)
 
-# Train set
-#test.d <- as.data.frame(as.matrix(DocumentTermMatrix(<test set here>, control = list(dictionary = lexicon))))
+# Test set
+
+#Merge Test sets
+overall.test <-Corpus(VectorSource(c(content(mistery.test), content(romance.test))))
+
+#test set with the same lexicon
+test.d <- as.data.frame(as.matrix(DocumentTermMatrix(overall.test, control = list(dictionary = lexicon))))
+
+#class labels for the test set
+test.c <- c(c(rep("Mistery", length(mistery.test)), c(rep("Romance", length(romance.test)))))
+
+
+#-----Classification
+#Dtree
+set.seed(565)
+dtree <- train (train.d, train.c, method ='rpart')
+
+#confusion matrix
+conf.mx.dtree <-table(test.c, predict(dtree, test.d))
+tp.dtree <- conf.mx [1,1]
+fp.dtree <- conf.mx [2,1]
+tn.dtree <- conf.mx [2,2]
+fn.dtree <- conf.mx [1,2]
 
